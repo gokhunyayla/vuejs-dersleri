28a9c2ea68ab5cd005c04291bb88c62f1663d220
Gökhun YAYLA <50734335+gokhunyayla@users.noreply.github.com>
2023-06-09 18:14:56 +0300

    Delete ders18 - vue files & root component/vue-playlist directory

--- ders18 - vue files & root component/vue-playlist/src/main.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-new Vue({
-  el: '#app',
-  render: h => h(App) //render metodu App içindeki herşeyi render edip root component'e #app'e taşır
-})
